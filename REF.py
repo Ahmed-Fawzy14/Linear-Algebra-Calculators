e85eac25790c601a9507d8389c63afa3d9087dc6
Ahmed Fawzy <ahmed-fawzy@aucegypt.edu>
2024-07-09 09:26:02 +0300

    Made more modular

--- REF.py
+++ REF.py
@@ -1,21 +1,15 @@
 import numpy as np
 import sympy as sp
 
-
-def getRREF(matrix, numberOfRows):
+def typeII_and_III(matrix,numberOfRows):
     x = sp.symbols('x')
-    pivots = []
-    numberOfRows, numberOfColumns = matrix.shape
-    
-    # The first non-zero entry of each row must be 1
+     # The first non-zero entry of each row must be 1
     for i in range(numberOfRows):
         first_nonZero = False  # Used to flag if we found the pivot
         for jth_column, entry in enumerate(matrix[i, :]):  # Traversing for every entry in the ith row (traversing columns of the ith row)
             if not first_nonZero:
                 # If this is a non-zero element, divide the entire row by it to make it a leading 1 and mark that we found the pivot
                 if entry != 0:
-                    # Use numpy.where to find the indices of the entry
-                    #pivots.append((i, jth_column))
                     matrix[i, :] /= entry
                     first_nonZero = True
 
@@ -34,14 +28,41 @@ def getRREF(matrix, numberOfRows):
             else:
                 break
 
-    # Replace -0.00 with 0.00
-    matrix[matrix == -0.0] = 0.0
+def typeII_and_III(matrix,identity_matrix,numberOfRows):
+    x = sp.symbols('x')
 
-    print("Going to enter")
-    #Type I: Put pivots in order
+    # The first non-zero entry of each row must be 1
+    for i in range(numberOfRows):
+        first_nonZero = False  # Used to flag if we found the pivot
+        for jth_column, entry in enumerate(matrix[i, :]):  # Traversing for every entry in the ith row (traversing columns of the ith row)
+            if not first_nonZero:
+                # If this is a non-zero element, divide the entire row by it to make it a leading 1 and mark that we found the pivot
+                if entry != 0:
+                    # Use numpy.where to find the indices of the entry
+                    matrix[i, :] /= entry
+                    identity_matrix[i, :] /= entry
+                    first_nonZero = True
 
- 
-    while True:
+                    
+
+                    # Type III: Make the other entries in this column zero
+                    for ith_row, column_entry in enumerate(matrix[:, jth_column]):  # Traversing in the column to eliminate other entries
+                        if (ith_row != i) and (column_entry != 0):
+                            equation = sp.Eq(matrix[ith_row][jth_column] + x * matrix[i, jth_column], 0)
+                            solution = sp.solve(equation, x)
+                            float_solution = float(solution[0])
+                            matrix[ith_row, :] += float_solution * matrix[i, :]
+                            identity_matrix[ith_row, :] += float_solution * identity_matrix[i, :]
+
+
+                else:
+                    pass
+            else:
+                break
+
+
+def typeI_pivots(matrix):
+     while True:
         pivot_row_indices, pivot_column_indices = np.where(matrix == 1)
         pivot_indices = list(zip(pivot_row_indices, pivot_column_indices))
 
@@ -67,76 +88,8 @@ def getRREF(matrix, numberOfRows):
         if not swapped:
             break  # If no swaps were made, exit the while loop
             #I have a pivot with a 1 which has a greater row but a less than column than pivots ABOVE it swap)
-    # Type I: Move zero rows to the bottom
-    while True:
-        #I need to add something like this in the top loop to update things
-        rowOfZeros = np.all(matrix == 0, axis=1)
-        ZeroRow_indices = np.where(rowOfZeros)[0]
-        rowOfNonZeros = np.any(matrix != 0, axis=1)
-        NonZeroRow_indices = np.where(rowOfNonZeros)[0]
 
-        swapped = False
-        for zero_index in ZeroRow_indices:
-            for nonZero_index in NonZeroRow_indices:
-                if zero_index < nonZero_index:
-                    # Perform the swap
-                    temp = matrix[zero_index, :].copy()  # Use copy to avoid referencing the same memory
-                    matrix[zero_index, :] = matrix[nonZero_index, :]
-                    matrix[nonZero_index, :] = temp
-                    swapped = True
-                    break  # Exit the inner loop to update indices
-            if swapped:
-                break  # Exit the outer loop to update indices
-
-        if not swapped:
-            break  # If no swaps were made, exit the while loop
-
-    # Printing the matrix using NumPy's print options
-    print("Reduced Row Echelon Form (RREF):")
-    np.set_printoptions(precision=2, suppress=True)
-    print(matrix)
-    
-    return matrix
-
-        
-def getInverse(matrix):
-    x = sp.symbols('x')
-    pivots = []
-    numberOfRows, numberOfColumns = matrix.shape
-    identity_matrix = np.identity((numberOfRows))
-
-    # The first non-zero entry of each row must be 1
-    for i in range(numberOfRows):
-        first_nonZero = False  # Used to flag if we found the pivot
-        for jth_column, entry in enumerate(matrix[i, :]):  # Traversing for every entry in the ith row (traversing columns of the ith row)
-            if not first_nonZero:
-                # If this is a non-zero element, divide the entire row by it to make it a leading 1 and mark that we found the pivot
-                if entry != 0:
-                    # Use numpy.where to find the indices of the entry
-                    pivots.append((i, jth_column))
-                    matrix[i, :] /= entry
-                    identity_matrix[i, :] /= entry
-                    first_nonZero = True
-
-                    
-
-                    # Type III: Make the other entries in this column zero
-                    for ith_row, column_entry in enumerate(matrix[:, jth_column]):  # Traversing in the column to eliminate other entries
-                        if (ith_row != i) and (column_entry != 0):
-                            equation = sp.Eq(matrix[ith_row][jth_column] + x * matrix[i, jth_column], 0)
-                            solution = sp.solve(equation, x)
-                            float_solution = float(solution[0])
-                            matrix[ith_row, :] += float_solution * matrix[i, :]
-                            identity_matrix[ith_row, :] += float_solution * identity_matrix[i, :]
-
-
-                else:
-                    pass
-            else:
-                break
-
-    # Replace -0.00 with 0.00
-    matrix[matrix == -0.0] = 0.0
+def typeI_pivots(matrix, identity_matrix):
     while True:
         pivot_row_indices, pivot_column_indices = np.where(matrix == 1)
         pivot_indices = list(zip(pivot_row_indices, pivot_column_indices))
@@ -168,7 +121,31 @@ def getInverse(matrix):
         if not swapped:
             break  # If no swaps were made, exit the while loop
 
-        #I have a pivot with a 1 which has a greater row but a less than column than pivots ABOVE it swap)
+def typeI_zeroRows(matrix):
+     while True:
+        #I need to add something like this in the top loop to update things
+        rowOfZeros = np.all(matrix == 0, axis=1)
+        ZeroRow_indices = np.where(rowOfZeros)[0]
+        rowOfNonZeros = np.any(matrix != 0, axis=1)
+        NonZeroRow_indices = np.where(rowOfNonZeros)[0]
+
+        swapped = False
+        for zero_index in ZeroRow_indices:
+            for nonZero_index in NonZeroRow_indices:
+                if zero_index < nonZero_index:
+                    # Perform the swap
+                    temp = matrix[zero_index, :].copy()  # Use copy to avoid referencing the same memory
+                    matrix[zero_index, :] = matrix[nonZero_index, :]
+                    matrix[nonZero_index, :] = temp
+                    swapped = True
+                    break  # Exit the inner loop to update indices
+            if swapped:
+                break  # Exit the outer loop to update indices
+
+        if not swapped:
+            break  # If no swaps were made, exit the while loop
+
+def typeI_zeroRows(matrix, identity_matrix):
     # Type I: Move zero rows to the bottom
     while True:
         rowOfZeros = np.all(matrix == 0, axis=1)
@@ -196,17 +173,57 @@ def getInverse(matrix):
         if not swapped:
             break  # If no swaps were made, exit the while loop
 
+
+def getRREF(matrix, numberOfRows):
+    x = sp.symbols('x')
+    numberOfRows, numberOfColumns = matrix.shape
+    typeII_and_III(matrix, numberOfRows)
+   
+    # Replace -0.00 with 0.00
+    matrix[matrix == -0.0] = 0.0
+
+    print("Going to enter")
+    #Type I: Put pivots in order
+    typeI_pivots(matrix)
+    # Type I: Move zero rows to the bottom
+    typeI_zeroRows(matrix)
+
+    # Printing the matrix using NumPy's print options
+    print("Reduced Row Echelon Form (RREF):")
+    np.set_printoptions(precision=10, suppress=True)
+    print(matrix)
+    
+    return matrix
+
+        
+def getInverse(matrix):
+    numberOfRows, numberOfColumns = matrix.shape
+    identity_matrix = np.identity((numberOfRows))
+ 
+    typeII_and_III(matrix, identity_matrix, numberOfRows)
+
+   
+    # Replace -0.00 with 0.00
+    matrix[matrix == -0.0] = 0.0
+
+    #Type I: Put pivots in order
+    typeI_pivots(matrix, identity_matrix)
+
+    # Type I: Move zero rows to the bottom
+    typeI_zeroRows(matrix, identity_matrix)
+
+
     # Replace -0.00 with 0.00
     matrix[matrix == -0.0] = 0.0
     identity_matrix[identity_matrix == -0.0] = 0.0
 
     # Printing the matrix using NumPy's print options
     print("Iverse Matrix: ")
-    np.set_printoptions(precision=2, suppress=True)
+    np.set_printoptions(precision=10, suppress=True)
     print(identity_matrix)
 
     print("Reduced Row Echelon Form (RREF):")
-    np.set_printoptions(precision=2, suppress=True)
+    np.set_printoptions(precision=10, suppress=True)
     print(matrix)
     
 
